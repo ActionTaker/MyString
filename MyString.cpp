185467720fa1c212f467e00ad3d40a27680f6137
JUHALK <92378476+JUHALK@users.noreply.github.com>
2021-10-14 14:44:59 +0000

    Complete MyString class and read me file.

---
+++ MyString.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+class MyString{
+  char* string; //pointer to string
+  int length;
+  int capacity;
+public:
+  MyString(const char* str);
+  MyString(const MyString& str);
+  
+  char operator[](int index);
+  void reserve(int size);
+  MyString& assign(const MyString& str);
+  MyString& insert(int index, const MyString& str);
+  MyString& erase(int from, int to);
+  MyString operator+(const MyString& str) const;
+  const MyString& operator=(const MyString& str);  
+
+  void print() const;
+  void println() const;
+  void printinfo() const;
+
+};
+MyString& MyString::erase(int index, int num)
+{
+  for(int i = index; i < length; i++)
+  {
+    string[i] = string[i+num];
+  }
+  length -= num;
+  return *this;
+}
+MyString& MyString::assign(const MyString& str)
+{
+  if(str.capacity > capacity)
+  {
+    delete[] string;
+    length = str.length;
+    capacity = str.capacity;
+    string = new char[capacity];
+    for(int i = 0; i < length; i++) string[i] = str.string[i];
+  }
+  else
+  {
+    length = str.length;
+    for(int i = 0; i < length; i++) string[i] = str.string[i];
+  }
+  return *this;
+}
+MyString::MyString(const char* str)
+{
+  length = capacity = strlen(str);
+  string = new char[capacity];
+  for(int i = 0; i < length; i++)
+    string[i] = str[i];
+}
+MyString::MyString(const MyString& str)
+{
+  length = str.length;
+  capacity = str.capacity;
+  string = new char[capacity];
+  for(int i = 0; i < length; i++)
+    string[i] = str.string[i];
+}
+
+char MyString::operator[](int index)
+{
+  return string[index];
+}
+
+MyString& MyString::insert(int index, const MyString& str)
+{
+  if(capacity < str.length + length)
+  {
+    if(2*capacity > str.length + length) 
+      capacity *= 2;
+    else
+      capacity = str.length + length;
+
+    char* new_string = new char[capacity];
+
+    for(int i = 0; i < length; i++)
+      new_string[i] = string[i];
+
+    delete[] string;
+    string = new_string;
+  }
+  for(int i = length-1; i >= index; i--) //주석달기
+    string[i + str.length] = string[i];
+
+  for(int i = 0; i < str.length; i++)
+    string[i + index] = str.string[i];
+  length += str.length;
+  return *this;
+}
+
+MyString MyString::operator+(const MyString& str) const
+{
+  MyString temp = *this;
+  temp.insert(length, str);
+  return temp;
+}
+
+const MyString& MyString::operator=(const MyString& str)
+{
+  return (*this).assign(str);
+}
+
+
+void MyString::print() const
+{
+  for(int i = 0; i < length; i++)
+    cout << string[i];
+}
+void MyString::println() const
+{
+  for(int i = 0; i < length; i++)
+    cout << string[i];
+  cout << '\n';
+}
+void MyString::printinfo() const
+{
+  cout << "MyString :"; println();
+  cout << "Length :" << length << '\n';
+  cout << "Capacity :" << capacity << '\n';
+}
+int main()
+{
+  MyString str = "ab";
+  MyString str1 = "c";
+  MyString str2 = str+str1; //abc
+  (str = str1);
+  str = str2 = str1;
+  str.printinfo();
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,90 @@
 # MyString
+``` 
+class MyString{
+  char* string; //pointer to string
+  int length;
+  int capacity;
+public:
+  MyString(const char* str);
+  MyString(const MyString& str);
+  
+  char operator[](int index);
+  void reserve(int size);
+  MyString& assign(const MyString& str);
+  MyString& insert(int index, const MyString& str);
+  MyString& erase(int from, int to);
+  MyString operator+(const MyString& str) const;
+  MyString& operator=(const MyString& str);  
+
+  void print() const;
+  void println() const;
+  void printinfo() const;
+};
+```
+# Note
+
+## MyString operator+(const MyString& str) const;
+>
++연산자는 피연산자, 즉 oper+ 함수를 가진 객체 그리고 매개변수로 받은 객체의 값을 바꾸지 않는다. 그러므로 const 함수로 선언 가능하고 const형으로 매개변수를 받을수있다. 반환형이 레퍼러스인경우는 보통 2가지이다. 매개변수를 레퍼런스로받아 그대로 리턴할경우 아니면 매개변수의 레퍼런스말고 객체 자신을 리턴할경우이다. +연산자의 경우 피연산자 두값(함수의주인객체, 매개변수 객체)을 더해서 새로운 객체를 리턴해야한다. 왜냐하면 +연산자는 피연산자에 영향을 끼치지 않기때문이다. 억지로 레퍼런스를 리턴하기위해서 함수내에서 
+```
+MyString& MyString::operator+(const MyString& str) const
+{
+  MyString temp = *this;
+  temp.insert(length, str);
+  return temp;
+}
+```
+라고 쓴다면 유효하지않다. b는 스택에 들어가서 함수종료후에 사라지고 레퍼런스의 값은 쓰레기값이 된다. 하지만 예를들어
+```
+MyString& MyString::operator+(const MyString& str) const
+{
+  MyString* temp = new MyString(*this);
+  temp.insert(length, str);
+  return temp;
+}
+```
+라고 작성하면 리턴값은 유효하지만 함수외부에서 동적할당 했으니 free 시켜줘야한다 그런복잡한 과정을 생략한것이
+```
+MyString MyString::operator+(const MyString& str) const
+{
+  MyString temp = *this;
+  temp.insert(length, str);
+  return temp;
+}
+```
+이다 이는 함수내에서 새로운 객채를만들고 그객체를 메모리상에 다시복사하여 임시객체를 만든다 그리고 그 임시객체의 레퍼런스를 반환한다. 그리고 그 레퍼런스를 사용하지않을때는 알아서 free 해준다.
+
+## MyString& operator=(const MyString& str);
+>
++operator 와 달리 새로운 객체가 필요하지않다 함수몸체주인객체의 값을 바꾸고 그 값을 함수 외부에서 복사, 혹은 읽기위해 적절하게 반환해야한다. 레퍼런스로 반환할수있고 그냥 일반형으로 반환할수 있지만 이경우에는 불필요한 임시객체의 생성을 막기위해 레퍼런스 타입으로 반환한다. 그 레퍼런스를 함수외부받는곳에서 바꾸기 싫다면, 예를들어 
+```
+MyString str = "ab";
+MyString str1 = "c";
+MyString str2 = str+str1; //abc
+(str = str1) = str2;
+```
+와같은 비상식적인 연산을 피고하싶다면 const MyString&를 반환형으로 취하면 된다. 그러면 불필요한 임시객체의 생성도 막고 그 레퍼런스를 순전히 읽기 용도로만 쓸수있다.
+```
+(str = str1) = str2;
+```
+대신에
+```
+str = str1;
+str = str2;
+```
+와같은 연산만 가능해진다.
+비슷한 윈리로 임시객체의 값을 함수외부에서 바꾸기 싫다면 const MyString 를 반환값으로 설정하면 된다. 하지만
+```
+str = str2 = str1;
+```
+와 같은행위는 여전히 가능하다. str2 = str1의 연산을 먼저 수행하고 상수화된 str2는 다시 str의 = 연산자의 매개변수가 되어 str2의 값은 바뀌지않기 때문이다. 하지만 여기서 = 연산자 매개변수형을 (const MyString& str) 에서 (MyString& str)로 변경한다면 
+```
+str = str2 = str1;
+```
+는 오류가난다. str2 = str1에서 str2은 상수화 되고 상수화된 str2는 MyString& str 의 매개변수가 될수없다.
+## 정리
+1. (str1 + str2) = str3 와같은 연산을 막아주는것, 즉 str1 + str2 의값을 상수화 시켜주는것은  +연산자의 반환값이다.
+2. str1를 상수화 시켜주는것은 함수
+3. str2를 상수화 시켜주는것은 매개변수.
+
+
